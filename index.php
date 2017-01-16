6114489e99ac58a4b160101cd180b5626b899a02
thesociallions <contact@thesociallions.nl>
2017-01-16 22:44:00 +0100

    Added "Hello, world!" example and created rewrite rule for HTTP traffic

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+/**
+ * CONTROLLER
+ */
+    require("vendor/autoload.php");
+
+    $database = new medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'hofstad',
+        'server' => 'localhost',
+        'username' => 'hofstad',
+        'password' => 'LR_hdh4@26', // TODO: Move to config file?
+        'charset' => 'utf8'
+    ]);
+    $templates = new League\Plates\Engine('view', 'tpl');
+    $router = new \Bramus\Router\Router();
+
+    $router->get("/", function (){
+        echo "<h1>Hello world!</h1>";
+    });
+
+    $router->run();
